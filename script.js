f413636665c334930ec9208cc785011d13ec9479
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-07-23 14:36:37 +0700

    add animation, fix no-meaning class name
    need to fix animation show then hide as well

--- script.js
+++ script.js
@@ -3,29 +3,41 @@ $(document).ready(function(){
 	$(".form-control").keyup(function(e){
 		var key = $(this).val();
 		var val = $.trim(key).replace(/ +/g, ' ').toLowerCase();
-		
-		
-		$rows.show().filter(function(){
-			var text = $(this).find(".name").text().replace(/\s+/g, ' ').toLowerCase();
-			var start = text.indexOf(val);
-			var m = $(this).find(".name").text();
-			$(this).find(".name").remove();
-			console.log(m);
-			//console.log(m[start]);
-			if(start != -1){
-				var text1 = m.substr(0,start);
-				var text2 = m.substr(start+key.length,m.length);
-				//console.log(text1 + "  " + key + "  " + text2);
-				$(this).find(".hi").remove();
-				
-				var div = '<div class="name">' + text1 + '<span class="hi">'+ key + '</span>' + text2 + '</div>';
-				//console.log(div);
-				$(this).find(".product-description").prepend(div);
-			}else{
-				var div = '<div class="name">' + m + '</div>';
-				$(this).find(".product-description").prepend(div);
-			}
-        return !~text.indexOf(val);
-    	}).hide();
+
+    	$rows.filter(function(){
+    	  var text = $(this).find(".name").text().replace(/\s+/g, ' ').toLowerCase(),
+    	      start = text.indexOf(val),
+    	      m = $(this).find(".name").text();
+    	  $(this).find(".name").remove();
+    	  if(start != -1){
+    	    var text1 = m.substr(0,start),
+    	        text2 = m.substr(start + key.length, m.length);
+    	    $(this).find(".search-highlight").remove();
+    	    var div = '<div class="name">' + text1 + '<span class="search-highlight">'+ key + '</span>' + text2 + '</div>';
+    	    $(this).find(".product-description").prepend(div);
+    	  } else {
+    	    var div = '<div class="name">' + m + '</div>';
+    	    $(this).find(".product-description").prepend(div);
+    	  }
+    	  return ~text.indexOf(val);
+    	}).show(400);
+
+    	$rows.filter(function(){
+    	  var text = $(this).find(".name").text().replace(/\s+/g, ' ').toLowerCase(),
+    	      start = text.indexOf(val),
+    	      m = $(this).find(".name").text();
+    	  $(this).find(".name").remove();
+    	  if(start != -1){
+    	    var text1 = m.substr(0,start),
+    	        text2 = m.substr(start + key.length, m.length);
+    	    $(this).find(".search-highlight").remove();
+    	    var div = '<div class="name">' + text1 + '<span class="search-highlight">'+ key + '</span>' + text2 + '</div>';
+    	    $(this).find(".product-description").prepend(div);
+    	  } else {
+    	    var div = '<div class="name">' + m + '</div>';
+    	    $(this).find(".product-description").prepend(div);
+    	  }
+    	  return !~text.indexOf(val);
+    	}).hide(400);
 	});
 });
