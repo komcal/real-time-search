338bd1ce12458c13e92a483644f888c80bb32d0a
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-07-23 14:41:36 +0700

    use .each instead of .filter

--- script.js
+++ script.js
@@ -3,41 +3,23 @@ $(document).ready(function(){
 	$(".form-control").keyup(function(e){
 		var key = $(this).val();
 		var val = $.trim(key).replace(/ +/g, ' ').toLowerCase();
-
-    	$rows.filter(function(){
-    	  var text = $(this).find(".name").text().replace(/\s+/g, ' ').toLowerCase(),
-    	      start = text.indexOf(val),
-    	      m = $(this).find(".name").text();
-    	  $(this).find(".name").remove();
-    	  if(start != -1){
-    	    var text1 = m.substr(0,start),
-    	        text2 = m.substr(start + key.length, m.length);
-    	    $(this).find(".search-highlight").remove();
-    	    var div = '<div class="name">' + text1 + '<span class="search-highlight">'+ key + '</span>' + text2 + '</div>';
-    	    $(this).find(".product-description").prepend(div);
-    	  } else {
-    	    var div = '<div class="name">' + m + '</div>';
-    	    $(this).find(".product-description").prepend(div);
-    	  }
-    	  return ~text.indexOf(val);
-    	}).show(400);
-
-    	$rows.filter(function(){
-    	  var text = $(this).find(".name").text().replace(/\s+/g, ' ').toLowerCase(),
-    	      start = text.indexOf(val),
-    	      m = $(this).find(".name").text();
-    	  $(this).find(".name").remove();
-    	  if(start != -1){
-    	    var text1 = m.substr(0,start),
-    	        text2 = m.substr(start + key.length, m.length);
-    	    $(this).find(".search-highlight").remove();
-    	    var div = '<div class="name">' + text1 + '<span class="search-highlight">'+ key + '</span>' + text2 + '</div>';
-    	    $(this).find(".product-description").prepend(div);
-    	  } else {
-    	    var div = '<div class="name">' + m + '</div>';
-    	    $(this).find(".product-description").prepend(div);
-    	  }
-    	  return !~text.indexOf(val);
-    	}).hide(400);
+		$rows.each(function(index, el) {
+			var text = $(el).find(".name").text().replace(/\s+/g, ' ').toLowerCase(),
+			    start = text.indexOf(val),
+			    m = $(el).find(".name").text();
+			$(el).find(".name").remove();
+			if(start != -1){
+			  var text1 = m.substr(0,start),
+			      text2 = m.substr(start + key.length, m.length);
+			  $(el).find(".search-highlight").remove();
+			  var div = '<div class="name">' + text1 + '<span class="search-highlight">'+ key + '</span>' + text2 + '</div>';
+			  $(el).find(".product-description").prepend(div);
+			  $(el).show(400);
+			} else {
+			  var div = '<div class="name">' + m + '</div>';
+			  $(el).find(".product-description").prepend(div);
+			  $(el).hide(400);
+			}
+		});
 	});
 });
